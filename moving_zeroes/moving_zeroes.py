e279d2586cf122d6aa209db49ce6ed1052c51fcd
Trevor Thompson <Thompson_Trevor@bah.com>
2020-07-16 19:26:41 -0400

    All of my moving zero tests pass

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,11 +1,18 @@
+import math
 '''
 Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
     # Your code here
-
-    return 0
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if abs(arr[j]) < abs(arr[i]):
+                temp_variable = arr[j]
+                arr[j] = arr[i]
+                arr[i] = temp_variable
+    
+    return arr
 
 
 if __name__ == '__main__':
