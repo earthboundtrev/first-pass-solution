46a1509b25377b12335b2307bb67ea89e83df52b
Trevor Thompson <Thompson_Trevor@bah.com>
2020-07-18 14:46:00 -0400

    I've successfully figured out how to solve the problem if input=2. Now I need to see if I can make it more modular, and then refactor it to make it not suck

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,23 +2,31 @@ import math
 # from itertools import permutations 
 # from itertools import combinations
 import itertools
-#!/usr/bin/python
-
+import ipdb
 
 import sys
 
 def rock_paper_scissors(n):
   # Your code here
   choices = ['rock', 'paper', 'scissors']
+  
+  iterating_list_counter = 0
+  max_iterating_list_size = pow(3, n)
+  list_to_return = [None] * max_iterating_list_size
   # choices = [1, 2, 3]
   # comb = itertools.combinations(choices, n)
   # print(list(itertools.combinations(choices, n)))
-  for index in range(0, n+1):
-      comb = itertools.combinations(['rock', 'paper', 'scissors'], index)
-      for i in list(comb):
-          perm = itertools.permutations(i)
-          for j in list(perm):
-              print(j)
+  for i in range(0, 3):
+    for j in range(0, 3):
+      # ipdb.set_trace()
+      list_as_iterating = []
+      print("This is the list before the value is updated:", list_as_iterating)
+      list_as_iterating.extend((choices[i],choices[j]))
+      list_to_return[iterating_list_counter] = list_as_iterating
+      iterating_list_counter=iterating_list_counter+1
+      print("This is the list after the value is updated:", list_as_iterating)
+    
+  return list_to_return
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
