04cccbb40012e1de2009433dbd3ff3994bb05123
Trevor Thompson <Thompson_Trevor@bah.com>
2020-07-23 17:13:36 -0400

    Implemented what I believe to be a 50% complete solution to the product problem

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,20 +1,30 @@
+# import ipdb
 '''
 Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
 
-    total = 1
+    copy_of_arr = arr
+    multiplied_arr = arr
+    counter = 0
 
-    for i in range(len(arr)):
-        total = total * arr[i]
+    for i in range(0, len(arr)):
+        # ipdb.set_trace()
+        
+        multiplied_arr = [element * arr[i] for element in multiplied_arr if i==counter]
+        print("This is i:", i, "This is counter:", counter)
+        counter=counter+1
+        print(multiplied_arr)
+    
+    for i in range(0, len(arr)):
+        arr[i] = multiplied_arr[i] / copy_of_arr[i]
 
-    return total
+    return arr
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 7, 3, 4]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -18,14 +18,32 @@ def rock_paper_scissors(n):
   # choices = [1, 2, 3]
   # comb = itertools.combinations(choices, n)
   # print(list(itertools.combinations(choices, n)))
+
+  if n<0:
+     return "Size of list cannot be lower than zero"
+  if n==0:
+     return []
+
   for i in range(0, max_iterating_list_size):
       list_to_iterate=[]
-      list_to_iterate.extend((choices[counter1], choices[counter2]))
-      if(counter2 < n):
-          counter2=counter2+1
-      else:
-          counter1=counter1+1
-          counter2=0
+      # list_to_iterate.extend((choices[counter1], choices[counter2]))
+      for j in range(n, 0):
+          if n==5:
+              list_to_iterate.extend((choices[counter_list[n]], choices[counter_list[n-1]], choices[counter_list[n-2]], choices[counter_list[n-3]], choices[counter_list[n-4]]))
+          elif n==4:
+              list_to_iterate.extend((choices[counter_list[n]], choices[counter_list[n-3]], choices[counter_list[n-2]], choices[counter_list[n-1]]))
+          elif n==3:
+              list_to_iterate.extend((choices[counter_list[n]], choices[counter_list[n-2]], choices[counter_list[n-1]]))
+          elif n==2:
+              list_to_iterate.extend((choices[counter_list[n]], choices[counter_list[n-1]]))
+          else:
+              list_to_iterate.extend((choices[counter_list[n]]))
+
+          if(counter_list[j] < n):
+              counter2=counter2+1
+          else:
+              counter1=counter1+1
+              counter2=0
       list_to_return[iterating_list_counter] = list_to_iterate
       iterating_list_counter=iterating_list_counter+1
       
