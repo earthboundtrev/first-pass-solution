08d31a100ac6c590177a83e9ad63c75890063391
Trevor Thompson <Thompson_Trevor@bah.com>
2020-07-23 20:08:41 -0400

    Successfully got tests to pass for product example

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -12,19 +12,21 @@ def product_of_all_other_numbers(arr):
     for i in range(0, len(arr)):
         # ipdb.set_trace()
         
-        multiplied_arr = [element * arr[i] for element in multiplied_arr if i==counter]
+        multiplied_arr = [element * arr[i] for element in multiplied_arr]
         print("This is i:", i, "This is counter:", counter)
+        multiplied_arr[i] = copy_of_arr[i]
+        copy_of_arr = multiplied_arr
         counter=counter+1
         print(multiplied_arr)
     
     for i in range(0, len(arr)):
-        arr[i] = multiplied_arr[i] / copy_of_arr[i]
+        multiplied_arr[i] = multiplied_arr[i] / arr[i]
 
-    return arr
+    return multiplied_arr
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 7, 3, 4]
-    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    # arr = [1, 7, 3, 4]
+    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
